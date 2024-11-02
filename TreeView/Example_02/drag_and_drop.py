dafbd45c4e32489543fd5c729b6c7aa55a76b14f
eike-zink <user@eike-zink.de>
2024-11-02 20:51:18 +0100

    Beispiel für Drag and Drop

---
+++ TreeView/Example_02/drag_and_drop.py
@@ -0,0 +1,32 @@
+import sys
+
+from PySide6 import QtWidgets, QtGui
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+
+        self.model = QtGui.QStandardItemModel()
+
+        for k in range(0, 4):
+            parent_item = self.model.invisibleRootItem()
+            for i in range(0, 4):
+                item = QtGui.QStandardItem(str(f'Item {k}.{i}'))
+                parent_item.appendRow(item)
+                parent_item = item
+
+        self.view = QtWidgets.QTreeView()
+        self.view.setModel(self.model)
+        self.view.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
+
+        self.setCentralWidget(self.view)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
---
+++ TreeView/Example_02/readme.md
@@ -0,0 +1,4 @@
+# Beispiel für das "Drag and Drop" innerhalb eines TreeView
+
+Grundlage ist eine Beitrag bei [stackoverflow.com](https://stackoverflow.com/questions/4163740/qtreeview-with-drag-and-drop-support-in-pyqt).
+
